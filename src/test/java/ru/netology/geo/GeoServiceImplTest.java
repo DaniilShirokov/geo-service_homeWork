54bfbd57687256eed5ceba071b3f80ec8f28a04e
Damiil Shirokov <daniil196196@gmail.com>
2024-05-29 00:49:50 +0500

    add test for method ByIp

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -14,31 +14,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GeoServiceImplTest {
-  GeoServiceImpl geoService = Mockito.mock(GeoServiceImpl.class);
+  //GeoServiceImpl geoService = Mockito.mock(GeoServiceImpl.class);
+  GeoServiceImpl geoService1 = new GeoServiceImpl();
 //  Mockito.when(geoService.byIp("127.0.0.1")).thenReturn(new Location(null,null,null,0));
 
-//    @ParameterizedTest
-//    @CsvSource( value = {
-//          "127.0.0.1", new Location(null,null,null,0),
+    @ParameterizedTest
+    @CsvSource( value = {
+          "127.0.0.1"
 //            "192.168.0.1",
 //            "172.0.32.11",
 //            "96.44.183.149",
 //            "172.0.64.16",
 //            "96.0.0.1"
-//   })
-//    void testbyIp(String ip , Location expected) {
-//        Assertions.assertEquals(expected,geoService.byIp(ip));
-//
-//    }
-//    @ParameterizedTest
-//    @CsvSource( value = {
-//
-//    }
+   })
+    void testbyIp(String ip , Location expected) {
+        Assertions.assertEquals(new Location(null,null,null,0),geoService1.byIp(ip));
+  }
+
     @Test
     void byCoordinates()  throws RuntimeException {
         Throwable exception = assertThrows(RuntimeException.class, () -> {
-            geoService.byCoordinates(10.0, 10.0);
+            geoService1.byCoordinates(10.0, 10.0);
         });
-        assertEquals("Not supported", exception.getMessage());
+        assertEquals("Not implemented", exception.getMessage());
     }
 }
